2c77f18735452b9eae805b2f6e81e9b081f6e391
JuanjoAvila <juanjo.avila.chavero@gmail.com>
2019-02-10 18:45:11 +0100

    Las clases añadidas

---
+++ ExtensionMethodExercice/ExtensionMethodExercice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Result());
+            Console.Read();
+
+            bool Result()
+            {
+                String hello = "Hello World";
+                bool result = hello.TotalWordsGreaterThan(10);
+                return result;
+            }
+
+
+        }
+    }
+}
---
+++ ExtensionMethodExercice/ExtensionMethodExercice/TotalString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExercice
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// El total de palabras
+        /// </summary>
+        /// <param name="hello"></param>
+        /// <param name="value"></param>
+        /// <returns>Calcula el total de palabras y asi sabe si es mas grande o mas pequeña</returns>
+        public static bool TotalWordsGreaterThan(this string hello, int value) => hello.Length > value;
+    }
+}
